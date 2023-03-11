355bbf4ccf5e596dfd27228c50ae3340734376ff
walkmanofficia <69844760+walkmanofficia@users.noreply.github.com>
2023-03-11 19:56:56 +0300

    Add files via upload

---
+++ models/Users.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('users', {
+		user_id: {
+			type: DataTypes.STRING,
+			primaryKey: true,
+		},
+		balance: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+			allowNull: false,
+		},
+		stawka: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+			allowNull: false,
+		},
+		stawk: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+			allowNull: false,
+		},
+	}, {
+		timestamps: false,
+	});
+};
